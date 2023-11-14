112a4c63f841fdb721745f3a5befc56af79b6a28
Nurr-ann <144720010+Nurr-ann@users.noreply.github.com>
2023-11-14 18:20:09 +0200

    pushh

--- utils.c
+++ utils.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * is_printable - Evaluates if a char is printable
+ * is_p - Evaluates if a char is printable
  * @c: Char to be evaluated.
  *
  * Return: 1 if c is printable, 0 otherwise
  */
-int is_printable(char c)
+int is_p(char c)
 {
 	if (c >= 32 && c < 127)
 		return (1);
@@ -16,23 +16,23 @@ int is_printable(char c)
 
 /**
  * append_hexa_code - Append ascci in hexadecimal code to buffer
- * @buffer: Array of chars.
+ * @buf: Array of chars.
  * @i: Index at which to start appending.
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
  */
-int append_hexa_code(char ascii_code, char buffer[], int i)
+int append_hexa_code(char ascii_code, char buf[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
 	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
-	buffer[i++] = '\\';
-	buffer[i++] = 'x';
+	buf[i++] = '\\';
+	buf[i++] = 'x';
 
-	buffer[i++] = map_to[ascii_code / 16];
-	buffer[i] = map_to[ascii_code % 16];
+	buf[i++] = map_to[ascii_code / 16];
+	buf[i] = map_to[ascii_code % 16];
 
 	return (3);
 }
@@ -52,13 +52,13 @@ int is_digit(char c)
 }
 
 /**
- * convert_size_number - Casts a number to the specified size
+ * convert_size_n - Casts a number to the specified size
  * @num: Number to be casted.
  * @size: Number indicating the type to be casted.
  *
  * Return: Casted value of num
  */
-long int convert_size_number(long int num, int size)
+long int convert_size_n(long int num, int size)
 {
 	if (size == S_LONG)
 		return (num);
@@ -69,13 +69,13 @@ long int convert_size_number(long int num, int size)
 }
 
 /**
- * convert_size_unsgnd - Casts a number to the specified size
+ * convert_size_uns - Casts a number to the specified size
  * @num: Number to be casted
  * @size: Number indicating the type to be casted
  *
  * Return: Casted value of num
  */
-long int convert_size_unsgnd(unsigned long int num, int size)
+long int convert_size_uns(unsigned long int num, int size)
 {
 	if (size == S_LONG)
 		return (num);
