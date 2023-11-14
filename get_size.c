f53df8145cb0a817d5e93e5be05b688d03cf1183
Nurr-ann <144720010+Nurr-ann@users.noreply.github.com>
2023-11-14 17:56:12 +0200

    pushh

--- get_size.c
+++ get_size.c
@@ -9,18 +9,18 @@
  */
 int get_size(const char *format, int *i)
 {
-	int curr_i = *i + 1;
+	int curi = *i + 1;
 	int size = 0;
 
-	if (format[curr_i] == 'l')
+	if (format[curi] == 'l')
 		size = S_LONG;
-	else if (format[curr_i] == 'h')
+	else if (format[curi] == 'h')
 		size = S_SHORT;
 
 	if (size == 0)
-		*i = curr_i - 1;
+		*i = curi - 1;
 	else
-		*i = curr_i;
+		*i = curi;
 
 	return (size);
 }
